2d42673a3c2dcb55eb8fa1f7c113fec33d010c00
Versus1478 <svider.vladyslav@gmail.com>
2026-06-02 22:22:11 +0200

    merge: resolve conflicts with main and fix App.vue type declarations

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// Тут буде ваш код скрипта
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Проект успішно запущено!</h1>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
@@ -22,20 +18,11 @@ header {
   line-height: 1.55;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
+    padding-right: 1rem;
   }
 
   header .wrapper {
